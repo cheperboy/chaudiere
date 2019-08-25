48d2062da9adce8c60bb86b9bb38b7043e6cc74f
cheperboy <matthieujouve@gmail.com>
2019-08-25 16:25:36 +0200

    local gui.sh UNIX format

---
+++ local_gui.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export DISPLAY=:0
+chromium-browser --app=http://localhost:5007/charts/
+
--- old_gui.sh
+++ old_gui.sh
@@ -1,10 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 # This script lauch chromium broser un kiosk mode
-
 export DISPLAY=:0
-
 #chromium-browser --kiosk --app=http://chaudiere.local:5007/charts/
 chromium-browser --app=http://localhost:5007/charts/
-
 # to kill this process
 # killall -9 chromium-browse

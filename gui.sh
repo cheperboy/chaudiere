107202bc5285291e36ef112f9954c1f5f002cb18
cheperboy <matthieujouve@gmail.com>
2019-08-25 15:57:46 +0200

    gui.sh not kiosk and url localhost

--- gui.sh
+++ gui.sh
@@ -3,7 +3,8 @@
 
 export DISPLAY=:0
 
-chromium-browser --kiosk --app=http://chaudiere.local:5007/charts/
+#chromium-browser --kiosk --app=http://chaudiere.local:5007/charts/
+chromium-browser --app=http://localhost:5007/charts/
 
 # to kill this process
 # killall -9 chromium-browse

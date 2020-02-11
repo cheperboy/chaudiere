eec4dc3fcc1590b127d740a309c4fccccfff7d62
cheperboy <matthieujouve@gmail.com>
2020-02-11 21:29:32 +0100

    modif local_gui.sh after ipv6 LAN router was deployed

--- local_gui.sh
+++ local_gui.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+
+# Get LAN IP (eg 192.168.0.70)
+# Open Chrome browser 
+
 export DISPLAY=:0
 
 # get local ip
 local_ip_raw=`hostname -I`
+# echo 'local_ip_raw ' -$local_ip_raw-
 
-#Remove leading whithespace
-local_ip="$(echo -e "${local_ip_raw}" | tr -d '[:space:]')"
+# Remove IPV6
+local_ip="$(echo -e "${local_ip_raw}" | cut -d ' ' -f1)"
+# echo 'local_ip ' -$local_ip-
 
-echo chromium-browser --kiosk --app=http://$local_ip:5007/charts/
+#echo chromium-browser --kiosk --app=http://$local_ip:5007/charts/
+chromium-browser --kiosk --app=http://$local_ip:5007/charts/
 
 # to kill this process
 # killall -9 chromium-browse
